b3402bc9a0bc694c8eafbfe8f28f42db2c121e53
PriyanshuRj <priyanshurajput0071109@gmail.com>
2022-06-04 00:31:24 +0530

    UPdated test cases

--- test/getreport.js
+++ test/getreport.js
@@ -4,8 +4,6 @@ let mongoose = require("mongoose");
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../index');
-let should = chai.should();
-
 
 chai.use(chaiHttp);
 //Our parent block
--- test/postreport.js
+++ test/postreport.js
@@ -2,8 +2,6 @@
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../index');
-let should = chai.should();
-
 
 chai.use(chaiHttp);
 //Our parent block
